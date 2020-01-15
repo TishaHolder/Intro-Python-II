879e9425b78c06969299b3b2451cf32ce0e83a14
Tisha Holder <50963507+TishaHolder@users.noreply.github.com>
2020-01-15 14:22:40 -0500

    added item type classes

--- src/item.py
+++ src/item.py
@@ -5,3 +5,44 @@ class Item:
 
     def __str__(self):
         return self.name + ': ' + self.description
+
+
+class Umbrella(Item):
+    def __init__(name, description, size):
+        super().__init__(name, description)
+        self.size = size
+
+    def __str__(self):
+        return self.name + ": " + self.description + ", rated: " + self.size
+
+class Binoculars(Item):
+    def __init__(name, description, magnification):
+        super().__init__(name, description)
+        self.magnification = magnification
+
+    def __str__(self):
+        return self.name + ": " + self.description + ", magnification:" + self.magnification
+
+class TreasureChest(Item):
+    def __init__(name, description, status):
+        super().__init__(name, description)
+        self.status = status
+
+    def __str__(self):
+        return self.name + ": " + self.description + ", status:" + self.status
+
+class Axe(Item):
+    def __init__(name, description, blade):
+        super().__init__(name, description)
+        self.blade = blade
+
+    def __str__(self):
+        return self.name + ": " + self.description + ", blade:" + self.blade
+
+class FlashLight(Item):
+    def __init__(name, description, brightness):
+        super().__init__(name, description)
+        self.brightness = brightness
+
+    def __str__(self):
+        return self.name + ": " + self.description + ", brightness:" + self.brightness
--- src/room.py
+++ src/room.py
@@ -10,6 +10,12 @@ class Room:
         self.description = description
         self.room_items = []    
 
+        #None is used to declare an attribute in Python without a value
+        #can overwrite None in a class method
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
 
     def __str__(self):
         return self.name + ': ' + self.description
